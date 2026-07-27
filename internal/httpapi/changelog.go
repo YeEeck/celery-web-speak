c463fb1441516af6e6284c3656ad905144c80d23
yeck <yechenkai@foxmail.com>
2026-07-27 09:11:41 +0800

    chore: 更新版本号到 v0.4.10

--- internal/httpapi/changelog.go
+++ internal/httpapi/changelog.go
@@ -9,7 +9,7 @@ import (
 
 // appVersion 是当前应用版本号。
 // 发版时需同步更新此常量与 web/package.json 的 version 字段。
-const appVersion = "0.4.9"
+const appVersion = "0.4.10"
 
 //go:embed CHANGELOG.json
 var changelogJSON []byte
